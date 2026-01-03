56bc8dfefe2206a75d0219381e95abaf83c17b50
𝐒𝐡𝐢𝐯𝐭𝐞𝐣 𝐏𝐚𝐰𝐚𝐫. <shivtejpawar8@gmail.com>
2026-01-03 10:12:32 +0530

    Time: 139 ms (5.01%), Space: 48 MB (6.93%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        while(i<nums.length){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+            i++;
+        }
+        while(j<nums.length){
+            nums[j]=0;
+            j++;
+        }
+        for(int val:nums){
+            System.out.print(val+" ");
+        }
+    }
+}
