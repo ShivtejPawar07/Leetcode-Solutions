1af297272ab52d17d43d5fbf658b037909e58b44
𝐒𝐡𝐢𝐯𝐭𝐞𝐣 𝐏𝐚𝐰𝐚𝐫. <shivtejpawar8@gmail.com>
2026-02-10 08:34:14 +0530

    Time: 297 ms (83.21%), Space: 46.5 MB (98.72%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,24 +1,44 @@
 class Solution {
     public int longestBalanced(int[] nums) {
-         int n = nums.length;
-        int maxLen = 0;
+        //  int n = nums.length;
+        // int maxLen = 0;
 
-        for (int i = 0; i < n; i++) {
-            Set<Integer> evenSet = new HashSet<>();
-            Set<Integer> oddSet = new HashSet<>();
+        // for (int i = 0; i < n; i++) {
+        //     Set<Integer> evenSet = new HashSet<>();
+        //     Set<Integer> oddSet = new HashSet<>();
 
-            for (int j = i; j < n; j++) {
-                if (nums[j] % 2 == 0) {
-                    evenSet.add(nums[j]);
-                } else {
-                    oddSet.add(nums[j]);
-                }
+        //     for (int j = i; j < n; j++) {
+        //         if (nums[j] % 2 == 0) {
+        //             evenSet.add(nums[j]);
+        //         } else {
+        //             oddSet.add(nums[j]);
+        //         }
 
-                if (evenSet.size() == oddSet.size()) {
-                    maxLen = Math.max(maxLen, j - i + 1);
+        //         if (evenSet.size() == oddSet.size()) {
+        //             maxLen = Math.max(maxLen, j - i + 1);
+        //         }
+        //     }
+        // }
+        // return maxLen;
+
+        int n=nums.length;
+        int maxlen=0;
+
+        for(int i=0;i<n;i++){
+            HashSet<Integer>ehs=new HashSet<>();
+             HashSet<Integer>ohs=new HashSet<>();
+
+            for(int j=i;j<n;j++){
+                if(nums[j]%2==0)
+                    ehs.add(nums[j]);
+                else
+                    ohs.add(nums[j]);
+                if (ehs.size() == ohs.size()) {
+                    maxlen = Math.max(maxlen, j - i + 1);
                 }
             }
+            
         }
-        return maxLen;
+        return maxlen;
     }
 }
