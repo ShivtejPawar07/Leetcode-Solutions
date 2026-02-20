da707f4dabdb49007c46a72b81e90261c33fc000
𝐒𝐡𝐢𝐯𝐭𝐞𝐣 𝐏𝐚𝐰𝐚𝐫. <shivtejpawar8@gmail.com>
2026-02-20 10:06:19 +0530

    Time: 2 ms (100%), Space: 43.3 MB (48.34%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> list = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') count++;
+            else count--;
+            
+            if (count == 0) {
+                // Recursively solve inner part
+                String inner = s.substring(start + 1, i);
+                list.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+        
+        // Sort descending
+        Collections.sort(list, Collections.reverseOrder());
+        
+        StringBuilder result = new StringBuilder();
+        for (String str : list) {
+            result.append(str);
+        }
+        
+        return result.toString();
+    }
+}
